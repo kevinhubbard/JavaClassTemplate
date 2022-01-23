f28ad23cf05e14b2829378ccc501c70bcd17f454
Kevin H <dgkallday98@gmail.com>
2022-01-23 14:14:38 -0500

    gui refactoring

--- Gui.java
+++ Gui.java
@@ -2,53 +2,49 @@ import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
 
-
 public class Gui {
 	JFrame frame = new JFrame("Create a Class");
 	JPanel panel = new JPanel();
+	
+	JCheckBox mainCb = new JCheckBox("Is main() class?");
+	JCheckBox importCb = new JCheckBox("Imports?");
+	JCheckBox extendCb = new JCheckBox("Extends?");
+	JCheckBox implementCb = new JCheckBox("Implements?");
+
+	JTextField classNameTf = new JTextField(10);
+	JTextField importTf = new JTextField(10);
+	JTextField extendTf = new JTextField(10);
+	JTextField implementTf = new JTextField(10);
+
+	JLabel classLbl = new JLabel("Class Name:");
+	JButton create = new JButton("Create");
+	String importString;
+	String extendString;
+	String implementString;
 
 	public Gui() {
-		JLabel classLbl = new JLabel("Class Name:");
-		JTextField className = new JTextField(10);
-
-		JLabel mainLbl = new JLabel("Is main() class?");
-		JCheckBox mainCb = new JCheckBox("Is main() class?");
-
-		JLabel importLbl = new JLabel("Imports?");
-		JCheckBox importCb = new JCheckBox("Imports?");
-		JTextField importTf = new JTextField(10);
-
-		JLabel extendLbl = new JLabel("Extends?");
-		JCheckBox extendsCb = new JCheckBox("Extends?");
-		JTextField extendTf = new JTextField(10);
-
-		JLabel implementLbl = new JLabel("Implements?");
-		JCheckBox implementCb = new JCheckBox("Implements?");
-		JTextField implementTf = new JTextField(10);
-
-		JButton create = new JButton("Create");
-		create.addActionListener(new SubmitListener());
-
 		panel.setBackground(Color.GRAY);
+
+		
 		panel.add(classLbl);
-		panel.add(className);
-
-		panel.add(mainLbl);
+		panel.add(classNameTf);
 		panel.add(mainCb);
-
-		panel.add(importLbl);
 		panel.add(importCb);
 		panel.add(importTf);
-
-		panel.add(extendLbl);
-		panel.add(extendsCb);
+		panel.add(extendCb);
 		panel.add(extendTf);
-
-		panel.add(implementLbl);
 		panel.add(implementCb);
 		panel.add(implementTf);
-
 		panel.add(create);
+
+		importTf.setEnabled(false);
+		extendTf.setEnabled(false);
+		implementTf.setEnabled(false);
+
+		importCb.addActionListener(new ImportsListener());
+		extendCb.addActionListener(new ExtendsListener());
+		implementCb.addActionListener(new ImplementsListener());
+		create.addActionListener(new SubmitListener());
 
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(350, 200);
@@ -58,7 +54,50 @@ public class Gui {
 
 	public class SubmitListener implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
-			System.out.println("submit was clicked.");
+			String className = classNameTf.getText();
+			boolean mainClass = mainCb.isSelected();
+
+			if (importCb.isSelected()) {
+				importString = importTf.getText();
+			}
+			if (extendCb.isSelected()) {
+				extendString = extendTf.getText();
+			}
+			if (implementCb.isSelected()) {
+				implementString = implementTf.getText();
+			}
+
+			System.out.println("class name was: " + className + "\nisMain() = " + mainClass + "\nimports: " + importString + "\nextends: " + extendString + "\nimplements: " + implementString);
+		}
+	}
+
+	public class ImportsListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			if (importCb.isSelected()) {
+				importTf.setEnabled(true);
+			} else {
+				importTf.setEnabled(false);
+			}
+		}
+	}
+
+	public class ExtendsListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			if (extendCb.isSelected()) {
+				extendTf.setEnabled(true);
+			} else {
+				extendTf.setEnabled(false);
+			}
+		}
+	}
+
+	public class ImplementsListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			if (implementCb.isSelected()) {
+				implementTf.setEnabled(true);
+			} else {
+				implementTf.setEnabled(false);
+			}
 		}
 	}
 }
