b52cc0a8b88417ee46a4b7affbf90823ff862d5f
Kevin H <dgkallday98@gmail.com>
2022-01-22 21:02:40 -0500

    gui updates

--- Gui.java
+++ Gui.java
@@ -8,7 +8,12 @@ public class Gui {
 		frame.setSize(650, 500);
 		JLabel lbl = new JLabel("Create a Class");
 		JPanel panel = new JPanel();
+		JPanel panel2 = new JPanel();
 		panel.setBackground(Color.GREEN);
+		panel.setSize(100,100);
+		panel2.setBackground(Color.PINK);
+		panel.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
+		panel.setBorder(BorderFactory.createLineBorder(Color.BLUE, 2));
 		panel.add(lbl);
 		frame.getContentPane().add(BorderLayout.CENTER, panel);
 		frame.setVisible(true);
