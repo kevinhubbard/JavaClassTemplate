e825da56a099aa8d35c94b18ad3407a9c7c176ae
Kevin H <dgkallday98@gmail.com>
2022-01-23 15:49:16 -0500

    refactored

--- Main.java
+++ Main.java
@@ -1,23 +1,4 @@
-import java.awt.*;
-import javax.swing.*;
-import java.util.*;
-import java.io.*;
-
 public class Main {
-
-	/*public void start() {
-
-		try {
-			PrintWriter outputFile = new PrintWriter(programName + EXT);
-			outputFile.print("public class " + programName + " {\n");
-			outputFile.print("\t public static void main(String [] args) {\n");
-			outputFile.print("\t\tSystem.out.println(\"Good Work Man.\");\n");
-			outputFile.print("\t}\n}");
-			outputFile.close();
-		} catch (IOException ex) {ex.printStackTrace();}
-	}*/
-
-
 	public static void main(String[] args) {
 		new Gui();
 	}
