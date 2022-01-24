bb31df43b66cf21589af0c14546449914201dd89
Kevin H <dgkallday98@gmail.com>
2022-01-24 17:15:04 -0500

    logic update

--- ClassConstructor.java
+++ ClassConstructor.java
@@ -1,35 +1,50 @@
 import java.io.*;
 
 public class ClassConstructor {
-	String cName;
-	String importSt;
-	String extendSt;
-	String implemSt;
 	String EXT = ".java";
-	boolean mainCl;
+	String className;
+	String importString;
+	String extendString;
+	String implemString;
+	boolean isMainClass;
 
-	public ClassConstructor(String name, boolean mainT) {
-		cName = name;
-		mainCl = mainT;
+	public ClassConstructor(String n, boolean m, String i, String e, String t) {
+		className = n;
+		isMainClass = m;
+		importString = i;
+		extendString = e;
+		implemString = t;
 		createFile();
 	}
 
 	public void createFile() {
-		if (mainCl) {
+		if (isMainClass) {
 			try {
-				PrintWriter outputFile = new PrintWriter(cName + EXT);
-				outputFile.print("public class " + cName + " {\n\n");
-				outputFile.print("\tpublic static void main(String[] args) {\n\n\t}\n}");
+				PrintWriter outputFile = new PrintWriter(className + EXT);
+				if (importString != null) {
+					outputFile.print("import " + importString + ";\n\n");
+				}
+				outputFile.print("public class " + className + " {\n\t\n");
+				outputFile.print("\tpublic static void main(String[] args) {\n\t\t\n\t}\n}");
 				outputFile.close();
 			} catch (IOException ex) {ex.printStackTrace();}
 			System.out.println("main class made");
 		} else {
 			try {
-				PrintWriter f = new PrintWriter(cName + EXT);
-				f.print("public class " + cName + "{\n\n}");
-				f.close();
+				PrintWriter outputFile = new PrintWriter(className + EXT);
+				if (importString != null) {
+					outputFile.print("import " + importString + ";\n\n");
+				}	
+				outputFile.print("public class " + className + " {\n\t\n}");
+				outputFile.close();
 			} catch (IOException ex) {ex.printStackTrace();}
 			System.out.println("not a main class");
 		}
+
+		System.out.println("Class Name: " + className);
+		System.out.println("Is main class: " + isMainClass);
+		System.out.println("Imports: " + importString);
+		System.out.println("Extends: " + extendString);
+		System.out.println("Implements: " + implemString);
 	}
 }
