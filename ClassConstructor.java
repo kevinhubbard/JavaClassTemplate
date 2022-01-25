bcd4592391f213fd9b1d2b5721cdaaa3532c43d8
Kevin H <dgkallday98@gmail.com>
2022-01-25 17:22:14 -0500

    logic and gui updates

--- ClassConstructor.java
+++ ClassConstructor.java
@@ -1,30 +1,68 @@
 import java.io.*;
+import java.util.*;
 
 public class ClassConstructor {
 	String EXT = ".java";
 	String className;
-	String importString;
-	String extendString;
-	String implemString;
+	//String importString;
+	String extendString = " extends ";
+	String implemString = " implements ";
 	boolean isMainClass;
 
-	public ClassConstructor(String n, boolean m, String i, String e, String t) {
+	ArrayList<String> importList = new ArrayList<String>();
+
+	public ClassConstructor(String n, boolean m, ArrayList<String> i, ArrayList<String> e, ArrayList<String> t) {
 		className = n;
 		isMainClass = m;
-		importString = i;
-		extendString = e;
-		implemString = t;
+		importList = i;
+		generateExtendString(e);
+		generateImplementString(t);
 		createFile();
+	}
+
+	public void generateExtendString(ArrayList<String> extendList) {
+		for (String parent : extendList) {
+			extendString += parent;
+		}
+	}
+
+	public void generateImplementString(ArrayList<String> interfaceList) {
+		for (String interfaceString : interfaceList) {
+			implemString += interfaceString + ", ";
+		}
+	}
+
+	public String insertExtendString() {
+		String te = "";
+		if (extendString.length() > 9) {
+			te = extendString;
+		} else {
+			System.out.println("Class did not extend anything.");
+		}
+		return te;
+	}
+
+	public String insertImplementString() {
+		String is = "";
+		if (implemString.length() > 12) {
+			is = implemString;
+		} else {
+			System.out.println("Class did not implement anything.");
+		}
+		return is;
 	}
 
 	public void createFile() {
 		if (isMainClass) {
 			try {
 				PrintWriter outputFile = new PrintWriter(className + EXT);
-				if (importString != null) {
-					outputFile.print("import " + importString + ";\n\n");
+				if (importList.size() != 0) {
+					for (String item : importList) {
+						outputFile.print(item + "\n");
+					}
+					outputFile.println();
 				}
-				outputFile.print("public class " + className + " {\n\t\n");
+				outputFile.print("public class " + className + insertExtendString() + insertImplementString() + "{\n\t\n");
 				outputFile.print("\tpublic static void main(String[] args) {\n\t\t\n\t}\n}");
 				outputFile.close();
 			} catch (IOException ex) {ex.printStackTrace();}
@@ -32,19 +70,16 @@ public class ClassConstructor {
 		} else {
 			try {
 				PrintWriter outputFile = new PrintWriter(className + EXT);
-				if (importString != null) {
-					outputFile.print("import " + importString + ";\n\n");
+				if (importList.size() != 0) {
+					for (String item : importList) {
+						outputFile.print(item + "\n");
+					}
+					outputFile.println();
 				}	
-				outputFile.print("public class " + className + " {\n\t\n}");
+				outputFile.print("public class " + className + insertExtendString() + insertImplementString() + "{\n\t\n}");
 				outputFile.close();
 			} catch (IOException ex) {ex.printStackTrace();}
 			System.out.println("not a main class");
 		}
-
-		System.out.println("Class Name: " + className);
-		System.out.println("Is main class: " + isMainClass);
-		System.out.println("Imports: " + importString);
-		System.out.println("Extends: " + extendString);
-		System.out.println("Implements: " + implemString);
 	}
 }
--- Gui.java
+++ Gui.java
@@ -4,27 +4,39 @@ import java.awt.event.*;
 import java.util.*;
 
 public class Gui {
-	String className, importString, extendString, implementString;
+	String className, importString, implementString;
 	boolean mainClass;
 	JFrame frame = new JFrame("Create a Class");
 	JPanel panel = new JPanel();
 	NamePanel classNamePanel = new NamePanel();
 	CbPanel checkBoxPanel = new CbPanel();
 	TfPanel textFieldPanel = new TfPanel();
+	BtnPanel addBtnPanel = new BtnPanel();
+	ArrayList<String> importList = new ArrayList<String>();
+	ArrayList<String> extendList = new ArrayList<String>();
+	ArrayList<String> implementList = new ArrayList<String>();
 
 	public Gui() {
 		panel.add(classNamePanel);
 		panel.add(checkBoxPanel);
 		panel.add(textFieldPanel);
-
+		panel.add(addBtnPanel);
 		JButton create = new JButton("Create");
 		panel.add(create);
+		addBtnPanel.importBtn.addActionListener(new AddImport());
+		addBtnPanel.extendBtn.addActionListener(new AddExtend());
+		addBtnPanel.implementBtn.addActionListener(new AddImplement());
+		checkBoxPanel.importCb.addActionListener(new ImportsListener());
+		checkBoxPanel.extendCb.addActionListener(new ExtendsListener());
+		checkBoxPanel.implementCb.addActionListener(new ImplementsListener());
 		create.addActionListener(new SubmitListener());
-
 		panel.setBackground(Color.GRAY);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(300, 250);
+		frame.setSize(330, 550);
+		frame.setResizable(false);
 		frame.getContentPane().add(BorderLayout.CENTER, panel);
+		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+		frame.setLocation((dim.width/2)-(frame.getWidth()/2), (dim.height/2)-(frame.getHeight()/2));
 		frame.setVisible(true);
 	}
 
@@ -37,33 +49,109 @@ public class Gui {
 		checkBoxPanel.importCb.setSelected(false);
 		checkBoxPanel.extendCb.setSelected(false);
 		checkBoxPanel.implementCb.setSelected(false);
-
 		className = null;
 		importString = null;
-		extendString = null;
 		implementString = null;
 		mainClass = false;
+		importList.clear();
+		extendList.clear();
+		implementList.clear();
+		textFieldPanel.importTf.setEnabled(false);
+		addBtnPanel.importBtn.setEnabled(false);
+		textFieldPanel.implementTf.setEnabled(false);
+		addBtnPanel.implementBtn.setEnabled(false);
+	}
+
+	public boolean classNameCheck(String cName) {
+		boolean testPass = false;
+		if (cName.length() == 0) {
+			JOptionPane.showMessageDialog(frame, "Invalid Class Name.");
+		} else {
+			testPass = true;
+		}
+		return testPass;
+	}
+
+	public class ImportsListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			if (checkBoxPanel.importCb.isSelected()) {
+				textFieldPanel.importTf.setEnabled(true);
+				addBtnPanel.importBtn.setEnabled(true);
+			} else {
+				textFieldPanel.importTf.setEnabled(false);
+				addBtnPanel.importBtn.setEnabled(false);
+			}
+		}
+	}
+
+	public class AddImport implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			String parseImport = "import " + textFieldPanel.importTf.getText() + ";";
+			importList.add(parseImport);
+			textFieldPanel.importTf.setText("");
+		}
+	}
+
+	public class ExtendsListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			if (checkBoxPanel.extendCb.isSelected()) {
+				textFieldPanel.extendTf.setEnabled(true);
+				addBtnPanel.extendBtn.setEnabled(true);
+			} else {
+				textFieldPanel.extendTf.setEnabled(false);
+				addBtnPanel.extendBtn.setEnabled(false);
+			}
+		}
+	}
+
+	public class AddExtend implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			if (extendList.size() != 0) {
+				String update = JOptionPane.showInputDialog(frame, "You can only extend one class.\nOverride previous parent or cancel");
+				if (update == null) {
+					System.out.println("nothing was input.  is still extended.");
+					textFieldPanel.extendTf.setText(extendList.get(0));
+				} else {
+					extendList.clear();
+					extendList.add(update);
+				}
+			} else {
+				extendList.add(textFieldPanel.extendTf.getText());
+			}
+			textFieldPanel.extendTf.setEnabled(false);
+			addBtnPanel.extendBtn.setEnabled(false);
+		}
+	}
+
+	public class ImplementsListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			if (checkBoxPanel.implementCb.isSelected()) {
+				textFieldPanel.implementTf.setEnabled(true);
+				addBtnPanel.implementBtn.setEnabled(true);
+			} else {
+				textFieldPanel.implementTf.setEnabled(false);
+				addBtnPanel.implementBtn.setEnabled(false);
+			}
+		}
+	}
+
+	public class AddImplement implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			implementList.add(textFieldPanel.implementTf.getText());
+			textFieldPanel.implementTf.setText("");
+		}
 	}
 
 	public class SubmitListener implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
 			className = classNamePanel.classNameTf.getText();
 			mainClass = checkBoxPanel.mainCb.isSelected();
-
-			if (checkBoxPanel.importCb.isSelected()) {
-				importString = textFieldPanel.importTf.getText();
+			if (classNameCheck(className)) {
+				new ClassConstructor(className, mainClass, importList, extendList, implementList);
+			} else {
+				System.out.println("error somewhere.");
 			}
-			if (checkBoxPanel.extendCb.isSelected()) {
-				extendString = textFieldPanel.extendTf.getText();
-			}
-			if (checkBoxPanel.implementCb.isSelected()) {
-				implementString = textFieldPanel.implementTf.getText();
-			}
-			
-			new ClassConstructor(className, mainClass, importString, extendString, implementString);
 			clearScreen();
 		}
 	}
-
-
 }
