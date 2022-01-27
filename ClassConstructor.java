0fce853c604f75ecce8a24a71b927ae9263fd9b1
Kevin H <dgkallday98@gmail.com>
2022-01-26 19:38:40 -0500

    logic updates (working with files)

--- ClassConstructor.java
+++ ClassConstructor.java
@@ -4,23 +4,36 @@ import java.util.*;
 public class ClassConstructor {
 	String EXT = ".java";
 	String className;
-	//String importString;
-	String extendString = "";
-	String implemString = "";
+	String importString;
+	String extendString;
+	String implemString;
 	boolean isMainClass;
+	File file;
 
-	ArrayList<String> importList = new ArrayList<String>();
-
-	public ClassConstructor(String n, boolean m, ArrayList<String> i, ArrayList<String> e, ArrayList<String> t) {
+	public ClassConstructor(String n, boolean m, ArrayList<String> i, ArrayList<String> e, ArrayList<String> t, File f) {
 		className = n;
 		isMainClass = m;
-		importList = i;
+		generateImportString(i);
 		generateExtendString(e);
 		generateImplementString(t);
+		file = f;
 		createFile();
 	}
 
+	public void generateImportString(ArrayList<String> imp) {
+		importString = "";
+		if (imp.size() == 0 ) {
+			System.out.println("this class doesnt import anything.");
+		} else {
+			for (String obj : imp) {
+				importString += "import " + obj + ";\n";
+			}
+			importString += "\n";
+		}
+	} 
+
 	public void generateExtendString(ArrayList<String> extendList) {
+		extendString = "";
 		if (extendList.size() == 0 ) {
 			System.out.println("this class does not extend anything.");
 		} else {
@@ -31,6 +44,7 @@ public class ClassConstructor {
 	}
 
 	public void generateImplementString(ArrayList<String> interfaceList) {
+		implemString = "";
 		if (interfaceList.size() == 0 ) {
 			System.out.println("this class does not implement any interfaces.");
 		} else {
@@ -50,13 +64,9 @@ public class ClassConstructor {
 	public void createFile() {
 		if (isMainClass) {
 			try {
+				
 				PrintWriter outputFile = new PrintWriter(className + EXT);
-				if (importList.size() != 0) {
-					for (String item : importList) {
-						outputFile.print(item + "\n");
-					}
-					outputFile.println();
-				}
+				outputFile.print(importString);
 				outputFile.print("public class " + className + extendString + implemString + " {\n\t\n");
 				outputFile.print("\tpublic static void main(String[] args) {\n\t\t\n\t}\n}");
 				outputFile.close();
@@ -64,13 +74,9 @@ public class ClassConstructor {
 			System.out.println("main class made");
 		} else {
 			try {
+				
 				PrintWriter outputFile = new PrintWriter(className + EXT);
-				if (importList.size() != 0) {
-					for (String item : importList) {
-						outputFile.print(item + "\n");
-					}
-					outputFile.println();
-				}	
+				outputFile.print(importString);	
 				outputFile.print("public class " + className + extendString + implemString + " {\n\t\n}");
 				outputFile.close();
 			} catch (IOException ex) {ex.printStackTrace();}
