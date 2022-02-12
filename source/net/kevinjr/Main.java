ae8bd611d8d89c5cf67d671bb250ad138fd6e350
Kevin H <dgkallday98@gmail.com>
2022-02-12 12:43:31 -0500

    main class now extends JFrame

--- source/net/kevinjr/Main.java
+++ source/net/kevinjr/Main.java
@@ -1,7 +1,22 @@
 package net.kevinjr;
 
-public class Main {
+import java.awt.*;
+import javax.swing.*;
+
+public class Main extends JFrame {
+
+	public void start() {
+		setTitle("Create A Class");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(330, 600);
+		setResizable(false);
+		getContentPane().add(BorderLayout.CENTER, new Gui());
+		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+		setLocation((dim.width/2)-(getWidth()/2), (dim.height/2)-(getHeight()/2));
+		setVisible(true);	
+	}
+	
 	public static void main(String[] args) {
-		new Gui();
+		new Main().start();
 	}
 }
