da057c5ada24ba24c68f2834e0fb909086e3f7fb
Kevin H <dgkallday98@gmail.com>
2022-03-01 19:55:51 -0500

    removed docs

--- docs/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"net.kevinjr","c":"Main","l":"createFrame()"},{"p":"net.kevinjr","c":"Main","l":"Main()","u":"%3Cinit%3E()"},{"p":"net.kevinjr","c":"Main","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
