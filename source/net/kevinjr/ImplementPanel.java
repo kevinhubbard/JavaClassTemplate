d386afce6b2ee1e3ce821c266c3187d4e2256485
Kevin H <dgkallday98@gmail.com>
2022-03-10 20:48:05 -0500

    implement panel updates

--- source/net/kevinjr/ImplementPanel.java
+++ source/net/kevinjr/ImplementPanel.java
@@ -9,32 +9,39 @@ public class ImplementPanel extends JPanel {
 	private JCheckBox box = new JCheckBox("Implements");
 	private JLabel label = new JLabel("Name: ");
 	private JTextField input = new JTextField(5);
+	private JButton btn = new JButton("Add");
 	private ArrayList<String> implementList = new ArrayList<String>();
 
 	public ImplementPanel() {
+		btn.setEnabled(false);
 		input.setEnabled(false);
 		box.addActionListener(new CheckBoxListener());
+		btn.addActionListener(new AddListener());
 		add(box);
 		add(label);
 		add(input);
+		add(btn);
 	}
 
 	public void clearFields() {
 		box.setSelected(false);
 		input.setText("");
 		input.setEnabled(false);
+		btn.setEnabled(false);
+		implementList.clear();
 	}
 
 	public ArrayList<String> getImplementList() {
 		return implementList;
 	}
 
-	private class UpdateListener implements ActionListener {
+	private class AddListener implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
 			if (input.getText() == null || input.getText().length() == 0) {
 				JOptionPane.showMessageDialog(null, "Invalid Implement");
 			} else {
 				implementList.add(input.getText());
+				input.setText("");
 			}
 		}
 	}
@@ -43,8 +50,10 @@ public class ImplementPanel extends JPanel {
 		public void actionPerformed(ActionEvent e) {
 			if (box.isSelected()) {
 				input.setEnabled(true);
+				btn.setEnabled(true);
 			} else {
 				input.setEnabled(false);
+				btn.setEnabled(false);
 			}
 		}
 	}
